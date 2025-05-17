277dc5511a31300b4804504a2ed1735fe773f79a
Adi <adimehta1970@gmail.com>
2025-05-18 05:25:57 +0530

    base integrations section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
 import Features from "@/sections/Features";
+import Integrations from "@/sections/Integrations";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <LogoTicker />
       <Introduction />
       <Features />
+      <Integrations />
     </>
   );
 }
