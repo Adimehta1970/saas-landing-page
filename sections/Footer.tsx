f46f3527e69b65ae9c1c93306e39e35b066b4795
Adi <adimehta1970@gmail.com>
2025-05-18 17:35:41 +0530

    footer check

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,3 +1,6 @@
+import Logo from "@/assets/images/logo.svg";
+import Image from "next/image";
+
 const footerLinks = [
   { href: "#", label: "Contact" },
   { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,28 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section className="py-16">
+      <div className="container sm:max-w-2xl md:max-w-6xl lg:max-w-full">
+        <div className="flex flex-col md:flex-row items-center md:justify-between  gap-6">
+          <div>
+            <Image src={Logo} alt="Layers logo" />
+          </div>
+          <div>
+            <nav className="flex gap-6">
+              {footerLinks.map((link, index) => (
+                <a
+                  key={index}
+                  href={link.href}
+                  className="text-white/50 text-sm"
+                >
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
